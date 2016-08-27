98eba9fa5f8bd84d2e225289c0a96849809e0478
Kaustav <kaustav@fsociety>
2016-08-27 15:10:17 +0530

    Update bug fixes

--- ReverseLinkedList.c
+++ ReverseLinkedList.c
@@ -10,41 +10,52 @@ struct node{
 
 void traverseLinkedList(struct node *linkedList);
 void interchangeNodes(struct node **firstNode, int key1, int key2);
+void reverseLinkedList(struct node **head);
+void swap(struct node **head, struct node *firstNodePrev, struct node *firstNode,
+  struct node *secondNodePrev, struct node *secondNode);
 
 int main(){
   struct node *head;
   struct node *second;
+  struct node *third;
   struct node *tail;
 
   head = (struct node *)malloc(sizeof(struct node));
   second = (struct node *)malloc(sizeof(struct node));
+  third = (struct node *)malloc(sizeof(struct node));
   tail = (struct node *)malloc(sizeof(struct node));
 
   head->data = 1;
   head->link = second;
 
   second->data = 2;
-  second->link = tail;
+  second->link = NULL;
 
-  tail->data = 3;
+  third->data = 3;
+  third->link = tail;
+
+  tail->data = 4;
   tail->link = NULL;
 
   traverseLinkedList(head);
-  interchangeNodes(&head, 3, 2);
+  reverseLinkedList(&head);
   traverseLinkedList(head);
 
   return 0;
 }
 
-void reverseLinkedList(struct node **head){
-  struct node *pointerNode = *head, *prevNode = NULL;
 
-  while(pointerNode->link != NULL){
+void reverseLinkedList(struct node **head){
+  struct node *pointerNode = *head;
+  struct node *prevNode = NULL;
+
+  while((pointerNode->link) != NULL){
     prevNode = pointerNode;
     pointerNode = pointerNode->link;
   }
 
-  swap(head, prevNode, *head, prevNode, pointerNode);
+  printf("[+] swap %p, %p, %p, %p \n", NULL, *head, prevNode, pointerNode);
+  swap(head, NULL, *head, prevNode, pointerNode);
 }
 
 
@@ -68,14 +79,17 @@ void swap(struct node **head, struct node *firstNodePrev, struct node *firstNode
   secondNode->link = firstNode->link;
   firstNode->link = temp;
 
+  traverseLinkedList(*head);
+
   struct node *pointerNode = *head;
 
-  while(pointerNode->link != secondNode){
+  while(pointerNode->link != firstNode){
     secondNodePrev = pointerNode;
     pointerNode = pointerNode->link;
   }
 
-  swap(head, firstNode, firstNode->link, secondNodePrev, pointerNode);
+  printf("[+] swap %p, %p, %p, %p \n", secondNode, secondNode->link, secondNodePrev, pointerNode);
+  swap(head, secondNode, secondNode->link, secondNodePrev, pointerNode);
 }
 
 
